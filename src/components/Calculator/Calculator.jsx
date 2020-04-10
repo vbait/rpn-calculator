61e60e827ef972e69a98df06afc661aab8402628
Vitalii Baitaliuk <vitalii.baitaliuk@agiliway.com>
2020-04-10 21:14:44 +0300

    add RPNCalculator service update Calculator component to work with RPNCalculator service add clear, clear all possibilities

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -1,46 +1,158 @@
-import React from 'react';
+import React, { useState, useCallback, useRef } from 'react';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Alert from 'react-bootstrap/Alert';
+import RPNCalculator, { Operator } from '../../services/RPNCalculator';
+import { NUMBERS } from './constants';
 import './styles.scss';
 
+const calculator = new RPNCalculator();
+calculator.addOperators([
+  new Operator('+', (a, b) => a + b),
+  new Operator('-', (a, b) => b - a),
+  new Operator('*', (a, b) => a * b),
+  new Operator('/', (a, b) => b / a),
+]);
+
+const separator = calculator.separator();
+
 function Calculator() {
-  const onChange = () => {};
-  const onAnd = () => {};
+  const inputRef = useRef();
+  const [operators] = useState(calculator.operators());
+  const [numbers, setNumbers] = useState(calculator.numbers());
+  const [value, setValue] = useState('');
+  const [error, setError] = useState();
+  const [readonly, setReadonly] = useState(false);
+
+  const handleReadonly = useCallback(() => {
+    setReadonly((v) => !v);
+  }, [setReadonly]);
+
+  const handleChange = useCallback(
+    (v) => {
+      const formatted = v.replace(/  +/g, ' ');
+      setError();
+      setValue(formatted);
+    },
+    [setValue, setError],
+  );
+
+  const handleAnd = useCallback(
+    (v) => {
+      setError();
+      setValue((state) => {
+        return state + v;
+      });
+      inputRef.current.focus();
+    },
+    [setValue, inputRef],
+  );
+
+  const handleApply = useCallback(() => {
+    try {
+      calculator.push(value.trim());
+      setValue('');
+      setNumbers(calculator.numbers());
+    } catch (err) {
+      setError(err.message);
+    }
+  }, [setError, setValue, setNumbers, value]);
+
+  const handleBackSpace = useCallback(() => {
+    setError();
+    setValue((state) => {
+      return state;
+    });
+    inputRef.current.focus();
+  }, [setError, setValue]);
+
+  const handleClear = useCallback(() => {
+    setValue('');
+    setError();
+  }, [setValue]);
+
+  const handleReset = useCallback(() => {
+    calculator.reset();
+    setValue('');
+    setError();
+    setNumbers(calculator.numbers());
+  }, [setValue]);
+
   return (
     <div className="calculator">
-      <div className="display">
-        <input type="text" onChange={onChange} className="form-control" />
+      {!!error && <Alert variant="danger">{error}</Alert>}
+      <div className="filter">
+        <Form.Check
+          type="checkbox"
+          id="id_readonly"
+          label="Readonly"
+          checked={readonly}
+          onChange={handleReadonly}
+        />
       </div>
       <div className="display">
-        <div className="form-control" />
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => handleChange(e.target.value)}
+          className="form-control"
+          readOnly={readonly}
+          ref={inputRef}
+        />
+      </div>
+      <div className="display">
+        <div className="form-control">{numbers.join(' ')}</div>
       </div>
       <div className="reset">
-        <Button variant="danger">CE</Button>
-        <Button variant="danger">C</Button>
+        <Button variant="danger" onClick={handleBackSpace}>
+          {'<-'}
+        </Button>
+        <Button variant="danger" onClick={handleClear}>
+          CE
+        </Button>
+        <Button variant="danger" onClick={handleReset}>
+          C
+        </Button>
       </div>
       <div className="calculator-container">
         <div className="numbers">
-          <Button onClick={() => onAnd(1)}>7</Button>
-          <Button>8</Button>
-          <Button>9</Button>
-          <Button>4</Button>
-          <Button>5</Button>
-          <Button>6</Button>
-          <Button>1</Button>
-          <Button>2</Button>
-          <Button>3</Button>
-          <Button className="double">0</Button>
-          <Button>.</Button>
+          {NUMBERS.map((v) => (
+            <Button key={v} onClick={() => handleAnd(v)}>
+              {v}
+            </Button>
+          ))}
+          <Button onClick={() => handleAnd(0)} className="double">
+            0
+          </Button>
+          <Button onClick={() => handleAnd('.')}>.</Button>
         </div>
         <div className="operations">
-          <Button variant="secondary">+</Button>
-          <Button variant="secondary">-</Button>
-          <Button variant="secondary">*</Button>
-          <Button variant="secondary">/</Button>
+          {operators.map((operator) => (
+            <Button
+              key={operator}
+              onClick={() => handleAnd(operator)}
+              variant="secondary"
+            >
+              {operator}
+            </Button>
+          ))}
         </div>
       </div>
       <div className="manipulate">
-        <Button variant="info">space</Button>
-        <Button variant="info">apply</Button>
+        <Button
+          onClick={() => handleAnd(separator)}
+          disabled={!value}
+          variant="info"
+        >
+          space
+        </Button>
+        <Button
+          onClick={handleApply}
+          disabled={!value || !!error}
+          variant="info"
+        >
+          apply
+        </Button>
       </div>
     </div>
   );
