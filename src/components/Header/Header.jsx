d5e93107e8b459fa8a0f158010447ea97855bcf9
Vitalii Baitaliuk <vitalii.baitaliuk@agiliway.com>
2020-04-10 14:40:36 +0300

    initialize app

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <nav className="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+      <a className="navbar-brand mr-auto mr-lg-0" href=".">
+        RPN Calculator
+      </a>
+    </nav>
+  );
+}
+
+export default Header;
